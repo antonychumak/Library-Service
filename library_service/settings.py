26023dede9925d56664e3ddbaae501c3dd134d72
Anton Chumak <Chumak7377@gmail.com>
2023-03-02 16:23:30 +0200

    Implement JWT support & 'Authorize' header

--- library_service/settings.py
+++ library_service/settings.py
@@ -131,3 +131,22 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+    "AUTH_HEADER_TYPES": (
+        "Bearer",
+        "JWP",
+    ),
+    "AUTH_HEADER_NAME": ("HTTP_AUTHORIZE",),
+}
