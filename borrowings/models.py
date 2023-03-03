8c52126bcbf967daae9f6408b263ec775531ceca
Anton Chumak <Chumak7377@gmail.com>
2023-03-03 10:34:37 +0200

    create & register Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, verbose_name="books")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="users")
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} - {self.book}"
