9ec11c6bc1704544cd6a9bf8a681fed3686a7155
Anton Chumak <Chumak7377@gmail.com>
2023-03-08 16:51:19 +0200

    Change field book for Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -11,15 +11,13 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
-    book = models.ForeignKey(
-        Book, on_delete=models.SET("expect delivery"), verbose_name="books"
-    )
+    book = models.ManyToManyField(Book, related_name="borrows")
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="users"
     )
 
     def __str__(self):
-        return f"{self.user} - {self.book}"
+        return f"{self.actual_return_date} - {self.expected_return_date}"
 
 
 class Payment:
