55da0e72ca6c99508d15192a686010b045f6271d
Anton Chumak <Chumak7377@gmail.com>
2023-03-16 15:26:25 +0200

    Write initadmin.py & wait_for_db function

---
+++ borrowings/management/commands/initadmin.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from django.conf import settings
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args: Any, **options: dict[str]) -> None:
+        if not User.objects.filter(email=settings.ADMINS["EMAIL"]).exists():
+            email = settings.ADMINS["EMAIL"]
+            password = settings.ADMINS["PASSWORD"]
+            print("Creating account for superuser")
+            admin = User.objects.create_superuser(email=email, password=password)
+            admin.save()
+        else:
+            print("Admin account has already been initialized.")
